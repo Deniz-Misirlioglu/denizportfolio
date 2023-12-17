e75aff9ff9991df98a098c75d6800c1b19d9336c
Deniz Misirlioglu <104803792+Deniz-Misirlioglu@users.noreply.github.com>
2023-12-17 01:53:02 -0500

    Commit Nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import StarsCanvas from "@/components/main/starbackground";
+import Navbar from "@/components/main/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll over-x-hidden`}
       >
+        <Navbar></Navbar>
+        <StarsCanvas></StarsCanvas>
         {children}
       </body>
     </html>
--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -3,14 +3,6 @@ import Herocontent from "../contents/herocontent";
 const hero = () => {
   return (
     <div className="relative flex flex-col h-full w-full">
-      <video
-        autoPlay
-        muted
-        loop
-        className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
-      >
-        <source src="/blackhole.webm" type="video/webm"></source>
-      </video>
       <Herocontent />
     </div>
   );
