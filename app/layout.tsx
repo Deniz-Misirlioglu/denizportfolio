5c5ba16cb21a9886e3869b1195481ed343027830
Deniz Misirlioglu <104803792+Deniz-Misirlioglu@users.noreply.github.com>
2024-06-26 02:18:29 -0400

    Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import StarsCanvas from "@/components/main/starbackground";
 import Navbar from "@/components/main/navbar";
 import Footer from "@/components/main/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <StarsCanvas></StarsCanvas>
         <Navbar></Navbar>
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
